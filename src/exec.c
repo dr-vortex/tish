8629c42461f0779df842312b13bf002028b7b19f
Dr. Vortex <jp@drvortex.dev>
2023-05-10 14:52:51 -0500

    Formatting

--- src/exec.c
+++ src/exec.c
@@ -3,10 +3,13 @@
 
 #include "shell.h"
 
-int get_command_code(char* command){
+int get_command_code(char *command)
+{
 	int command_code = -1;
-	for(size_t i = sizeof(commands) / sizeof(commands[0]) - 1; i > 0; i--){
-		if(!strcmp(command, commands[i])){
+	for (size_t i = sizeof(commands) / sizeof(commands[0]) - 1; i > 0; i--)
+	{
+		if (!strcmp(command, commands[i]))
+		{
 			command_code = i;
 			break;
 		}
@@ -14,17 +17,18 @@ int get_command_code(char* command){
 	return command_code;
 }
 
-void parse_args(char* string, char* result[]){
+void parse_args(char *string, char *result[])
+{
 
-    char* token;
-    int i = 0;
+	char *token;
+	int i = 0;
 
 	token = strtok(string, CMD_ARG_DELIM);
 	strcpy(result[i], token);
 
-    while(token != NULL) {
+	while (token != NULL)
+	{
 		token = strtok(NULL, CMD_ARG_DELIM);
 		strcpy(result[++i], token);
 	}
-
 }
--- src/main.c
+++ src/main.c
@@ -11,22 +11,25 @@
 #include "shell.h"
 
 char output[MAX_OUTPUT_SIZE] = "";
-char* input_prefix = INPUT_DELIM;
+char *input_prefix = INPUT_DELIM;
 
-//update the terminal
-int update(char* new_output){
+// update the terminal
+int update(char *new_output)
+{
 
-	#ifdef NOT_TI
+#ifdef NOT_TI
 	system("clear");
-	#else
+#else
 	os_ClrHome();
-	#endif
-	if(strlen(output) > MAX_OUTPUT_SIZE){
+#endif
+	if (strlen(output) > MAX_OUTPUT_SIZE)
+	{
 		printf("Error: Max output size exceeded");
 		return 1;
 	}
 
-	if(new_output){
+	if (new_output)
+	{
 		strcat(output, new_output);
 		strcat(output, "\n");
 	}
@@ -36,27 +39,28 @@ int update(char* new_output){
 	return 0;
 }
 
-int prompt(){
+int prompt()
+{
 
 	char input[MAX_INPUT_SIZE];
 	char input_text[MAX_INPUT_SIZE];
 	printf("%s", input_prefix);
 	fgets(input, MAX_INPUT_SIZE, stdin);
-	input[strcspn(input, "\n\r")] = 0; //remove trailing newline
+	input[strcspn(input, "\n\r")] = 0; // remove trailing newline
 
 	strcpy(input_text, input_prefix);
 	strcat(input_text, input);
-	
+
 	update(input_text);
 
 	int command_code = get_command_code(input);
 
-	switch(command_code){
-		case ST_EXIT:
-			break;
-		default:
-			update("Command does not exist");
-
+	switch (command_code)
+	{
+	case ST_EXIT:
+		break;
+	default:
+		update("Command does not exist");
 	}
 
 	return command_code;
@@ -66,10 +70,12 @@ int main()
 {
 	update(NULL);
 
-	while (true) {
+	while (true)
+	{
 		int state = prompt();
 
-		if(state == ST_EXIT){
+		if (state == ST_EXIT)
+		{
 			break;
 		}
 	}
